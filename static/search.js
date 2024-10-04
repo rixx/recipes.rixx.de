cef6a627465cc40830f84a009422ceb0230b8c62
Tobias Kunze <r@rixx.de>
2024-10-04 20:44:26 +0200

    Make search instantaneous

--- static/search.js
+++ static/search.js
@@ -1,65 +1,44 @@
-let recipeData = null
-let dataLoading = false
-
-
-const renderSearchResults = (recipes) => {
-    // build HTML
-    console.log(recipes)
-    const wrapper = document.querySelector("#search-container")
-    const div = document.createElement("div")
-    div.id = "search-results"
-    if (!recipes.length && !tags.length) {
-        div.classList.add("empty")  // allows us to always replace, because we're lazy like that
-    } else {
-        let content = ""
-        recipes.forEach(recipe => {
-            content += `<a href="/${recipe.id}/" class="result recipe-result">`
-            if (recipe.cover) {
-                content += `<img class="cover" src="/${recipe.id}/${recipe.cover}">`
-            } else {
-                content += `<img class="cover" src="/static/cover.png">`
-            }
-            content += `<div class="search-content"><div class="recipe-title">${recipe.title}</div></div></a>`
-        })
-        div.innerHTML = content
-    }
-    wrapper.replaceChild(div, wrapper.querySelector("#search-results"))
-}
+let hasCarousels = false
 
 const updateSearch = () => {
-    const searchInput = document.querySelector("input#search")
-    const search = searchInput.value
+    const search = document.querySelector("input#search").value
 
     const searchTerms = search
         .split(" ")
         .filter(d => d.length)
         .map(d => d.toLowerCase().trim())
 
-    if (!searchTerms.length) return renderSearchResults([], [])
-
-    const recipeHits = recipeData.recipes.filter(recipe => recipe.search.filter(d => searchTerms.some(term => d.includes(term))).length)
-    renderSearchResults(recipeHits)
-}
-
-const loadRecipeData = () => {
-    if (recipeData) {
-        return
+    if (!searchTerms.length) {
+        document.querySelectorAll(".recipe-card").forEach(card => card.classList.remove("hidden"))
+    } else {
+        document.querySelectorAll(".recipe-card").forEach(card => {
+            const match = searchTerms.every(term => card.textContent.toLowerCase().includes(term))
+            if (match) {
+                card.classList.remove("hidden")
+            } else {
+                card.classList.add("hidden")
+            }
+        })
     }
-    dataLoading = true  // todo render
 
-    fetch("/search.json").then(response => response.json().then(data => {
-        recipeData = data
-        dataLoading = false  // todo undo render
-        const searchInput = document.querySelector("input#search")
-        searchInput.removeEventListener("focus", loadRecipeData)
-        searchInput.addEventListener("input", updateSearch)
-        updateSearch()
-    }))
+    // Hide carousels if there are no results
+    if (hasCarousels) {
+        document.querySelectorAll(".recipe-carousel").forEach(carousel => {
+            const visibleCards = carousel.querySelectorAll(".recipe-card:not(.hidden)")
+            if (visibleCards.length === 0) {
+                carousel.classList.add("hidden")
+                carousel.previousElementSibling.classList.add("hidden")
+            } else {
+                carousel.classList.remove("hidden")
+                carousel.previousElementSibling.classList.remove("hidden")
+            }
+        })
+    }
 }
 
 const init = () => {
-    const searchInput = document.querySelector("input#search")
-    searchInput.addEventListener("focus", loadRecipeData)
+    document.querySelector("input#search").addEventListener("input", updateSearch)
+    hasCarousels = document.querySelectorAll(".recipe-carousel").length > 0
 }
 
 if (document.readyState !== "loading") { init() } else {
